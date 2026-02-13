17c47bb60a3ac6816384edc510a216443bbaa94b
Worasuda9 <worasuda.n@gmail.com>
2026-02-14 00:17:00 +0700

    edit code

--- index.js
+++ index.js
@@ -83,7 +83,14 @@ app.put('/api/todos/:id', (req, res) => {
     return res.status(404).json({ error: 'Todo not found' });
   }
   
-  todos[todoIndex].completed = true;
+  // Toggle completion status
+  todos[todoIndex].completed = !todos[todoIndex].completed;
+
+  if (writeTodos(todos)) {
+    return res.status(200).json(todos[todoIndex]);
+  } else {
+    return res.status(500).json({ error: 'Failed to update todo' });
+  }
 });
 
 // Delete a todo
